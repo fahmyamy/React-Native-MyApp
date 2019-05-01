673367ee2c446d44570568b3af08a12f65a1081f
mohd fahmi <fahmyamyp18@gmail.com>
2019-05-01 19:47:04 +0800

    Initial commit

---
+++ App.js
@@ -0,0 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import Main from './app/components/Main';
+
+  export default class App extends React.Component {
+  render() {
+    return (
+      <Main />
+      );
+  }
+}
